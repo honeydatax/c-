2270c323e54b5e952572a04d617ac5c344c7bd16
lisbon <16432726+honeydatax@users.noreply.github.com>
2018-11-07 08:15:00 +0000

    this is a assembly x86 compiler

---
+++ compiler.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Diagnostics;
+
+
+namespace compiler
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			String a;
+			String b;
+			Console.WriteLine ("file asm to compile");
+			Console.Beep ();
+			a=Console.ReadLine ();
+			Console.WriteLine ("file com file to output");
+			Console.Beep ();
+			b=Console.ReadLine ();
+			
+			ProcessStartInfo psi = new ProcessStartInfo();
+			psi.FileName = "/usr/bin/nasm";
+			psi.UseShellExecute = false;
+			psi.Arguments = "-o  " + b + "  " + a + "";
+			psi.RedirectStandardOutput = true;
+			Process p = Process.Start(psi);
+			Console.WriteLine(p.StandardOutput.ReadToEnd());
+			p.WaitForExit();
+			p.Close();
+			
+
+			
+			
+		}
+	}
+}
