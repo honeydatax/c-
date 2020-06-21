2636e17aa13832e89e84e78a9903bf3de90cf9cb
lisbon <16432726+honeydatax@users.noreply.github.com>
2020-06-21 19:49:15 +0100

    circles screen txt

--- circlescreentxt.cs
+++ circlescreentxt.cs
@@ -6,31 +6,27 @@ namespace logic{
 		public class maps{
 			public int count=25;
 			public int col=80;
-			private char [] screen= null;
+
 			
 
 			public maps(int x, int y){
 				int i;
 				count=y;
 				col=x;
-				screen= new char[col*count];
-				for (i=0;i<(col*count);i++) screen[i]=' ';
 			}
 	
-			public void  mapstar(int x,int y,char c){
-				if (x>-1 && x<col && y>-1 && y<count)screen[y*col+x]=c;
+			public void  mapstar(int x,int y,string ssss){
+				
+				if (x>-1 && x<col && y>-1 && y<count){
+					Console.SetCursorPosition(x,y);
+					Console.Write(ssss);
+				}
 			}
 			
 			public void Println(){
 				int i;
 				int ii;
 				string s="";
-				for (ii=0;ii<count;ii++){
-					s="";
-					for (i=0;i<col;i++)s=s+screen[ii*col+i].ToString();
-					Console.WriteLine("{0}",s);
-				}
-
 			}
 			
 			public void pstring(int x,int y,string s){
@@ -38,18 +34,21 @@ namespace logic{
 				int ii=x;
 				int iii=s.Length;
 				if (ii+iii>col)iii=iii+x-col;
-				for(i=0;i<iii;i++){
-					mapstar(i+x,y,s[i]);
-				}
+				
+					mapstar(x,y,s);
+				
 			}
 			public void vstring(int x,int y,string s){
 				int i=0;
 				int ii=y;
 				int iii=s.Length;
+				string ssss="";
 				if (ii+iii>count)iii=iii+y-count;
 				for(i=0;i<iii;i++){
-					mapstar(x,y+i,s[i]);
+					ssss=s[i]+"\r\n";
 				}
+					mapstar(x,y,ssss);
+				
 			}
 			public void vcenter(int x,string s){
 				int y=count/2;
@@ -80,30 +79,48 @@ namespace logic{
 				for (i=0;i<iii;i++){
 					d=xx+rr*Math.Cos(Convert.ToDouble(i)/(ddd)*Math.PI);
 					dd=yy+rr*Math.Sin(Convert.ToDouble(i)/(ddd)*Math.PI);
-					mapstar(Convert.ToInt16(d),Convert.ToInt16(dd),s);
+					mapstar(Convert.ToInt16(d),Convert.ToInt16(dd),s.ToString());
 				}
 
 			}
 			public void horline(int x, int y, int w , char c){
 				int i=x;
 				int ii=w;
+				string ssss="";
 				for (i=x;i<x+w;i++){
-					mapstar(i,y,c);
+					ssss=ssss+c;
 				}
+				mapstar(x,y,ssss);
 			}
 			public void fillcircle(int x,int y,int r,char s){
+				int [] half=new int[2000];
+				double d=0.00f;
+				double dd=0.00f;
+				double ddd=0.00f;
+				double d1=0.00f;
+				double ddd1=0.00f;
+				double rr=Convert.ToDouble(r);
+				string ss="";
+
 				int i=0;
-				for (i=0;i<r;i++){
-					circle(x,y,i,s);
+				ddd1=rr*2.00f;
+				ddd=ddd1*2.00f;
+				
+				
+				for (i=0;i<r*2;i++){
+					half[i]=Convert.ToInt16(rr*Math.Cos(Convert.ToDouble(i)/ddd1*Math.PI));
+					horline(x-half[i],i+(y-r),half[i]*2,s);
 				}
 				
 			}
 			public void verline(int x, int y, int h , char c){
 				int i=x;
 				int ii=h;
+				string ssss="";
 				for (i=y;i<y+h;i++){
-					mapstar(x,i,c);
+					ssss=ssss+c+"\r\n";
 				}
+				mapstar(x,y,ssss);
 			}
 			public void grid(int x, int y, int w ,int h,int steep ,char c){
 				int i=0;
@@ -131,9 +148,9 @@ namespace logic{
 		static void Main(string[] args){
 			
 			int ii=0;
-			
+			Console.Clear();
 			maps mp = new maps(80,20);
-			mp.grid(0,0,mp.col,mp.count,5,'>');
+			mp.grid(0,0,mp.col,mp.count,5,'.');
 			mp.lrect(1,1,10,10,',');
 			mp.rect(30,5,20,10,':');
 			mp.fillcircle((mp.col/2),(mp.count)/2,8,'-');
